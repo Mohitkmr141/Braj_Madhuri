e7b43e28ca66dfa06f0009cf0022b2556576d934
Mohit <mohitcod141@gmail.com>
2026-07-15 15:42:34 +0530

    fix vercel build for api

--- src/app/api/admin/orders/route.js
+++ src/app/api/admin/orders/route.js
@@ -1,7 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
-
 const prisma = new PrismaClient();
+
+export const dynamic = 'force-dynamic';
 
 export async function GET() {
   try {
